712432941cbb4575d79139b17a464aa380cec7c4
esandorfi <e.sandorfi@gmail.com>
2024-12-08 13:21:49 +0100

    fix: remove noise from ai generated component :)

--- layouts/test.vue
+++ layouts/test.vue
@@ -1,7 +1,6 @@
 <template>
     <h1>test</h1>
     <PrimeMenubar></PrimeMenubar>
-    <GeneratedNavbar></GeneratedNavbar>
     <div
         class="max-w-2xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg">
         <main
